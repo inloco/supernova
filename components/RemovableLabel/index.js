be30cc4b48957d7533d260b6d4c958520f16a850
Kauê Buriti <kaue.buriti@inlocomedia.com>
2018-11-16 13:58:21 -0300

    fix(removable-label): apply the onClick to the hole area that contains the icon

--- components/RemovableLabel/index.js
+++ components/RemovableLabel/index.js
@@ -5,8 +5,8 @@ export const RemovableLabel = props => {
   const { children, className = '', content, onRemove, ...otherProps } = props
   return (
     <Label className={`removable ${className}`} {...otherProps}>
-      <div className="removable-label-icon">
-        <Icon className="clear" size="big" onClick={onRemove} />
+      <div className="removable-label-icon" onClick={onRemove}>
+        <Icon className="clear" size="big" />
       </div>
       <span className="removable-label-content">{children || content}</span>
     </Label>
