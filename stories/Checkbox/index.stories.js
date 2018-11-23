c01c423f0815b3d58efd245a31cdddaab0799fe2
Maira Bello <maira.bello@inlocomedia.com>
2018-11-23 14:14:43 -0300

    Style the Checkbox toggle component

--- stories/Checkbox/index.stories.js
+++ stories/Checkbox/index.stories.js
@@ -37,3 +37,26 @@ storiesOf('Checkbox', module)
       <Checkbox className="green" label="Radio Green" radio />
     </React.Fragment>
   ))
+  .add('radio disabled', () => (
+    <React.Fragment>
+      <Checkbox label="Checked disabled" radio disabled defaultChecked />
+      <br /><br />
+      <Checkbox label="Unchecked disabled" radio disabled />
+    </React.Fragment>
+  ))
+  .add('toggle', () => (
+    <React.Fragment>
+      <Checkbox className="blue" label="Blue" toggle />
+      <br /><br />
+      <Checkbox className="pink" label="Pink" toggle />
+      <br /><br />
+      <Checkbox className="green" label="Green" toggle />
+    </React.Fragment>
+  ))
+  .add('toggle disabled', () => (
+    <React.Fragment>
+      <Checkbox label="Checked disabled" toggle disabled defaultChecked />
+      <br /><br />
+      <Checkbox label="Unchecked disabled" toggle disabled />
+    </React.Fragment>
+  ))
