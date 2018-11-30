cf02695ffac72bc7af24d47e5db2086a836f85fd
Maira Bello <maira.bello@inlocomedia.com>
2018-11-30 15:31:28 -0300

    Add story for a vertical menu

--- stories/Menu/index.stories.js
+++ stories/Menu/index.stories.js
@@ -1,37 +1,53 @@
-import React from "react";
-import { Menu } from "semantic-ui-react";
-import { storiesOf } from "@storybook/react";
+import React from 'react'
+import { Icon, Menu } from 'semantic-ui-react'
+import { storiesOf } from '@storybook/react'
 
 const items = [
   {
-    content: "TAB #1"
+    content: 'TAB #1'
   },
   {
-    content: "TAB #2"
+    content: 'TAB #2'
   },
   {
-    content: "TAB #3"
+    content: 'TAB #3'
   }
 ];
 
-storiesOf("Menu", module)
-  .add("basic pointing ", () => <Menu pointing items={items} />)
-  .add("secondary pointing ", () => <Menu secondary pointing items={items} />)
-  .add("basic pointing blue", () => (
+const verticalItems = [
+  {
+    content: 'Home',
+    icon: 'home'
+  },
+  {
+    content: 'Places',
+    icon: 'place'
+  },
+  {
+    content: 'Reports',
+    icon: 'assignment'
+  }
+]
+
+storiesOf('Menu', module)
+  .add('vertical', () => <Menu vertical items={verticalItems} />)
+  .add('basic pointing ', () => <Menu pointing items={items} />)
+  .add('secondary pointing ', () => <Menu secondary pointing items={items} />)
+  .add('basic pointing blue', () => (
     <Menu color="blue" pointing items={items} />
   ))
-  .add("secondary pointing blue", () => (
+  .add('secondary pointing blue', () => (
     <Menu secondary pointing color="blue" items={items} />
   ))
-  .add("basic pointing pink", () => (
+  .add('basic pointing pink', () => (
     <Menu pointing color="pink" items={items} />
   ))
-  .add("secondary pointing  pink", () => (
+  .add('secondary pointing  pink', () => (
     <Menu secondary pointing color="pink" items={items} />
   ))
-  .add("basic pointing green", () => (
+  .add('basic pointing green', () => (
     <Menu color="green" pointing items={items} />
   ))
-  .add("secondary pointing green", () => (
+  .add('secondary pointing green', () => (
     <Menu secondary pointing color="green" items={items} />
   ));
