62fcd34f069ccea870ce36e17caaa0543767b247
Maira Bello <maira.bello@inlocomedia.com>
2018-12-07 08:57:03 -0300

    Should allow Layout without a sidebar
    This is used on logged out pages, for example.

--- components/Layout/Sidebar/index.js
+++ components/Layout/Sidebar/index.js
@@ -1,7 +1,7 @@
 import cx from 'classnames'
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
-import { Accordion, Icon, Menu } from 'semantic-ui-react'
+import { Accordion, Dimmer, Icon, Menu } from 'semantic-ui-react'
 
 class Sidebar extends Component {
   static propTypes = {
@@ -25,15 +25,22 @@ class Sidebar extends Component {
     const classes = cx('inloco-layout__sidebar', color, { expanded })
     const headerIcon = expanded ? 'close' : 'menu'
     return (
-      <Menu as={Accordion} className={classes} vertical icon={!expanded}>
-        <Menu.Item header>
-          <Icon className={headerIcon} onClick={this.handleHeaderIconClick} />
-          {expanded && headerTitle}
-        </Menu.Item>
-        {React.Children.map(children, child =>
-          React.cloneElement(child, { expanded })
-        )}
-      </Menu>
+      <React.Fragment>
+        <Menu as={Accordion} className={classes} vertical icon={!expanded}>
+          <Menu.Item header>
+            <Icon className={headerIcon} onClick={this.handleHeaderIconClick} />
+            {expanded && headerTitle}
+          </Menu.Item>
+          {React.Children.map(children, child =>
+            React.cloneElement(child, { expanded })
+          )}
+        </Menu>
+        {/* <Dimmer
+          className="inloco-layout__sidebar-dimmer"
+          active={expanded}
+          page
+        /> */}
+      </React.Fragment>
     )
   }
 
